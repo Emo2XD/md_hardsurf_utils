a6c2ee468f7196889df1abd1a1231adc1dc91ffd
Emo2XD <emo2xd@gmail.com>
2025-03-08 10:48:18 +0900

    normal transfer UI changed

--- tools/ui.py
+++ tools/ui.py
@@ -21,7 +21,7 @@ class MDHARD_PT_md_hard(bpy.types.Panel):
         
 
         layout = self.layout
-        layout.operator(ot.MDHARD_OT_test_x.bl_idname, text="Test X")
+        # layout.operator(ot.MDHARD_OT_test_x.bl_idname, text="Test X")
         row = layout.row(align=True)
         split = row.split(factor=0.8, align=True)
         split.operator(ot.MDHARD_OT_setup_part_collection.bl_idname, text="Setup Part", icon="OUTLINER_COLLECTION")
@@ -29,9 +29,7 @@ class MDHARD_PT_md_hard(bpy.types.Panel):
 
         
 
-        layout.separator()
-        layout.label(text="Dual Normal Transfer")
-        layout.operator(ot.MDHARD_OT_sync_dnt.bl_idname, text="Sync DNT", icon="FILE_REFRESH")
+        
         # layout.operator(ot.MDHARD_OT_update_dnt.bl_idname, text="Update DNT", icon="FILE_REFRESH")
 
 
@@ -42,7 +40,7 @@ class MDHARD_PT_md_normal_transfer(bpy.types.Panel):
     """Panel for normal transfer
     """
     bl_idname = "MDHARD_PT_md_normal_transfer" 
-    bl_label = "Normal Transfer"
+    bl_label = "Part Operators"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "MD Hard" # tab name
@@ -57,13 +55,15 @@ class MDHARD_PT_md_normal_transfer(bpy.types.Panel):
             part_collection = None
 
         layout = self.layout
+
         if part_collection is not None:
-            layout.label(text=f"Active Part: {part_collection.name}")
+            layout.label(text=f"Active Part: {part_collection.name}", icon="OUTLINER_COLLECTION")
             layout.prop(part_collection, ct.NORMAL_TRANSFER_SRC_OBJ_PER_COLLECTION, text="Source")
             layout.operator(ot.MDHARD_OT_normal_transfer.bl_idname, text="Normal Transfer", icon="MOD_DATA_TRANSFER")
         else:
             layout.label(text=f"Active Part: None")
-            
-            
+
+        layout.separator()
+        layout.operator(ot.MDHARD_OT_sync_dnt.bl_idname, text="Sync DNT", icon="FILE_REFRESH")    
 
         return
